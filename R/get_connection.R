82e4debf04cc887f6836494dcb4f77d93ba2a234
Máté Kovács <mate.kovacs@alphacruncher.com>
2020-02-13 17:08:50 +0100

    Restructuring for three possible signatures

--- R/get_connection.R
+++ R/get_connection.R
@@ -2,94 +2,108 @@
 #'
 #' Function get_connection(dbname, schemaname)
 #' Creates a new connection to schema dbname.schemaname. If both arguments are NULL then get_connection will try to call get_nuvolos_db_path().
-#'
+#' 
+#' @param username The Nuvolos username of the connecting user. To obtain the username, please consult the connection guide on Nuvolos.
+#' @param password The Nuvolos password of the connecting user. To obtain the password, please consult the connection guide on Nuvolos.
 #' @param dbname The database (organization + space) to connect to
 #' @param schemaname The schema (instance + state) to connect to
+#' @return Return an ODBC connection object.
+#'
+#' @examples
+#' con <- get_connection()
+#' con <- get_connection(dbname = "my_database", schemaname = "my_schema")
+#' con <- get_connection(username = "my_user", password = "my_password", dbname = "my_database", schemaname = "my_schema")
+#'
 #' @export
-get_connection <- function(dbname = NULL, schemaname = NULL) {
-  require(DBI)
-	
-	if (is.null(dbname) && is.null(schemaname)) {
-		conn_info <- get_nuvolos_db_path()
-		dbname <- conn_info$dbname
-		schemaname <- conn_info$schemaname
-	} else if (is.null(dbname) && !is.null(schemaname)) {
-		stop("Inconsistent input: dbname is NULL but schemaname is not. Please specify a dbname argument or leave both arguments NULL.")
-	} else if (!is.null(dbname) && is.null(schemaname)) {
-		stop("Inconsistent input: dbname is not NULL but schemaname is. Please specify a schemaname argument or leave both arguments NULL.")
-	} else {
-	}
+get_connection <- function(username = NULL, password = NULL, dbname = NULL, schemaname = NULL) {
+   require(DBI)
+   
+   # Only accept dbname and schemaname together. If both are missing resort to relying on get_nuvolos_db_path() to figure it out. 
+   if (is.null(dbname) && is.null(schemaname)) {
+      conn_info <- get_nuvolos_db_path()
+      dbname <- conn_info$dbname
+      schemaname <- conn_info$schemaname
+   } else if (is.null(dbname) && !is.null(schemaname)) {
+      stop("Inconsistent input: dbname is NULL but schemaname is not. Please specify a dbname argument or leave both arguments NULL.")
+   } else if (!is.null(dbname) && is.null(schemaname)) {
+      stop("Inconsistent input: dbname is not NULL but schemaname is. Please specify a schemaname argument or leave both arguments NULL.")
+   } else {
+   }
 
-	path_user <- '/secrets/username'
-	path_token <- '/secrets/snowflake_access_token'
+   # Only accept username and password together. If both are missing, resort to relying on using Kube secret files to substitute.
+   if (is.null(username) && is.null(password) {
 
-  if (file.exists("~/.odbc.ini")) {
-	 print("Using odbc.ini file.")
-    inifile <- ini::read.ini('~/.odbc.ini')
+      path_user <- '/secrets/username'
+      path_token <- '/secrets/snowflake_access_token'
 
-    username <- inifile$nuvolos$uid
-    password <- inifile$nuvolos$pwd
-  } else if (file.exists(path_user) && file.exists(path_token) ) {
-	print("Using secret files.")
-	con_user <- file(path_user, "r")
-	line_user <- readLines(con_user, n = 1, warn = FALSE)
-	close(con_user)
+      if (file.exists(path_user) && file.exists(path_token) ) {
+         print("Using secret files.")
+         con_user <- file(path_user, "r")
+         line_user <- readLines(con_user, n = 1, warn = FALSE)
+         close(con_user)
 
-	if( length(line_user) == 0)
-		stop(paste0('Could not parse username file, first line of ', path_user, ' is empty.'))
-	username <- line_user
+         if( length(line_user) == 0)
+            stop(paste0('Could not parse username file, first line of ', path_user, ' is empty.'))
+         username <- line_user
 
-	con_pw <- file(path_token, "r")
-	line_pw <- readLines(con_pw, n = 1, warn = FALSE)
-	close(con_pw)
-	
-	if (length(line_pw) == 0 )
-		stop(paste0('Could not parse token file, first line of ', path_token ,' is empty.'))
-	password <- line_pw
-  } else {
-    username <- rstudioapi::askForSecret("Nuvolos Username:")
-    password <- rstudioapi::askForSecret("Nuvolos Token (not your password!):")
-  }
-  sysname  <- Sys.info()["sysname"]
+         con_pw <- file(path_token, "r")
+         line_pw <- readLines(con_pw, n = 1, warn = FALSE)
+         close(con_pw)
 
-  if (sysname == "Linux") {
-    con <- odbc::dbConnect(odbc::odbc(),
-                           uid=username,
-                           pwd=password,
-                           driver="SnowflakeDSIIDriver",
-                           server="alphacruncher.eu-central-1.snowflakecomputing.com",
-                           database=dbname,
-                           schema=schemaname,
-                           warehouse=username,
-                           role=username,
-                           tracing=0)
+         if (length(line_pw) == 0 )
+            stop(paste0('Could not parse token file, first line of ', path_token ,' is empty.'))
+         password <- line_pw
+      } else {
+         stop("You are not using a Nuvolos application. Please provide both username and password arguments to establish a connection.")
+      }
+  
 
-  } else if (sysname == "Windows") {
-    con <- odbc::dbConnect(odbc::odbc(),
-                           uid = username,
-                           pwd = password,
-                           driver = "SnowflakeDSIIDriver",
-                           server = "alphacruncher.eu-central-1.snowflakecomputing.com",
-                           database = dbname,
-                           schema = schemaname,
-                           warehouse = username,
-                           role = username,
-                           tracing = 0)
+   } else if(is.null(username) && !is.null(password) {
+      stop("Inconsistent input: username is NULL, but password was provided. Please specify a username or leave both arguments as NULL.")
+   } else if(!is.null(username) && is.null(password) {
+      stop("Inconsistent input: password is NULL, but username was provided. Please specify a password or leave both arguments as NULL.")
+   }
 
-  } else if (sysname == "Darwin") {
-    con <- odbc::dbConnect(odbc::odbc(),
-                           uid=username,
-                           pwd=password,
-                           driver="/opt/snowflake/snowflakeodbc/lib/universal/libSnowflake.dylib",
-                           server="alphacruncher.eu-central-1.snowflakecomputing.com",
-                           database=dbname,
-                           schema=schemaname,
-                           warehouse=username,
-                           role=username,
-                           tracing=0)
-  }
-  options(odbc.batch_rows = 10000)
-  return(con)
+   sysname  <- Sys.info()["sysname"]
+
+   if (sysname == "Linux") {
+   con <- odbc::dbConnect(odbc::odbc(),
+                  uid=username,
+                  pwd=password,
+                  driver="SnowflakeDSIIDriver",
+                  server="alphacruncher.eu-central-1.snowflakecomputing.com",
+                  database=dbname,
+                  schema=schemaname,
+                  warehouse=username,
+                  role=username,
+                  tracing=0)
+
+   } else if (sysname == "Windows") {
+      con <- odbc::dbConnect(odbc::odbc(),
+                  uid = username,
+                  pwd = password,
+                  driver = "SnowflakeDSIIDriver",
+                  server = "alphacruncher.eu-central-1.snowflakecomputing.com",
+                  database = dbname,
+                  schema = schemaname,
+                  warehouse = username,
+                  role = username,
+                  tracing = 0)
+
+   } else if (sysname == "Darwin") {
+      con <- odbc::dbConnect(odbc::odbc(),
+                  uid=username,
+                  pwd=password,
+                  driver="/opt/snowflake/snowflakeodbc/lib/universal/libSnowflake.dylib",
+                  server="alphacruncher.eu-central-1.snowflakecomputing.com",
+                  database=dbname,
+                  schema=schemaname,
+                  warehouse=username,
+                  role=username,
+                  tracing=0)
+   }
+   options(odbc.batch_rows = 10000)
+   return(con)
 }
 
 #' Function get_nuvolos_db_path()
@@ -98,24 +112,24 @@ get_connection <- function(dbname = NULL, schemaname = NULL) {
 #'
 #' @export
 get_nuvolos_db_path <- function() {
-  path_filename <- Sys.getenv("ACLIB_DBPATH_FILE", "/lifecycle/.dbpath")
-  if (!file.exists(path_filename))
-    stop(paste0('Could not find dbpath file ', path_filename, '.'))
+   path_filename <- Sys.getenv("ACLIB_DBPATH_FILE", "/lifecycle/.dbpath")
+   if (!file.exists(path_filename))
+      stop(paste0('Could not find dbpath file ', path_filename, '.'))
 
-  con = file(path_filename, "r")
-  line = readLines(con, n = 1, warn = FALSE)
-  close(con)
+   con = file(path_filename, "r")
+   line = readLines(con, n = 1, warn = FALSE)
+   close(con)
 
-  if (length(line) == 0)
-    stop(paste0('Could not parse dbpath file, first line of ', path_filename, ' is empty.'))
+   if (length(line) == 0)
+      stop(paste0('Could not parse dbpath file, first line of ', path_filename, ' is empty.'))
 
-  split_arr <- unlist(strsplit(line, "\".\""))
-  if (length(split_arr) != 2 )
-    stop(paste0("Invalid path format in dbpath file ", path_filename, '.'))
+   split_arr <- unlist(strsplit(line, "\".\""))
+   if (length(split_arr) != 2 )
+      stop(paste0("Invalid path format in dbpath file ", path_filename, '.'))
 
-  db_name <- paste0(split_arr[1],"\"")
-  schema_name <- paste0("\"",split_arr[2])
-  sprintf("Found database = %s, schema = %s in dbpath file %s.", db_name, schema_name, path_filename)
+   db_name <- paste0(split_arr[1],"\"")
+   schema_name <- paste0("\"",split_arr[2])
+   sprintf("Found database = %s, schema = %s in dbpath file %s.", db_name, schema_name, path_filename)
 
-  return(list(dbname = db_name, schemaname = schema_name))
+   return(list(dbname = db_name, schemaname = schema_name))
 }
