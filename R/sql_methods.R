0c6b423d8f38628b4fbe91009bfa040ad9b6030c
Bence Pipis <bencepipis@Bences-MacBook-Pro.local>
2021-04-20 13:48:24 +0200

    simplification in sql_methods package import

--- R/sql_methods.R
+++ R/sql_methods.R
@@ -1,10 +1,11 @@
+require(reticulate)
+
+#imoporting python-based nuvolos connector package and pandas
+nuvolos <- import("nuvolos")
+pd <- import("pandas")
+
 dbGetQuery <- function(sql, con){
-  require(reticulate)
-  
-  #imoporting python-based nuvolos connector package
-  nuvolos <- import("nuvolos")
-  pd <- import("pandas")
-  
+
   # using python's pandas.read_sql() method 
   return(pd$read_sql(sql, con))
 }
@@ -18,21 +19,16 @@ dbWriteTable <- function(dbname,
                          index=TRUE,
                          index_label=NULL,
                          nanoseconds=FALSE){
-  require(reticulate)
-  
-  #imoporting python-based nuvolos connector package
-  nuvolos <- import("nuvolos")
-  
+
   # using nuvolos python package's to_sql() method
   return(nuvolos$to_sql(df=dbname, name=name, con=con, database=database, schema=schema,
                  if_exists="fail", index=TRUE, index_label=NULL, nanoseconds=FALSE))
 }
 
-dbExecute <- function(statement, con){
-  require(reticulate)
-  
-  # using python's execute method on the established connecton
-  return(con$execute(statement))
+dbExecute <- function(sql, con){
+
+  # using python's execute method on the established connection
+  return(con$execute(sql))
 }
 
 
