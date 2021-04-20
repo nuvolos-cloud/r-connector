d7b1b4ec0d79d714450ba9f4fdb752fbd23c1773
Bence Pipis <bencepipis@Bences-MacBook-Pro.local>
2021-04-20 14:56:41 +0200

    attempt to use sql_methods functions-v2

--- R/sql_methods.R
+++ R/sql_methods.R
@@ -1,16 +1,13 @@
-require(reticulate)
 
-#imoporting python-based nuvolos connector package and pandas
-
-#' @export
-nuvolos <- import("nuvolos")
-
-#' @export
-pd <- import("pandas")
 
 #' @export
 dbGetQuery <- function(sql, con){
-
+  require(reticulate)
+  
+  #imoporting python-based nuvolos connector package and pandas
+  nuvolos <- import("nuvolos")
+  pd <- import("pandas")
+  
   # using python's pandas.read_sql() method 
   return(pd$read_sql(sql, con))
 }
@@ -25,7 +22,12 @@ dbWriteTable <- function(dbname,
                          index=TRUE,
                          index_label=NULL,
                          nanoseconds=FALSE){
-
+  require(reticulate)
+  
+  #imoporting python-based nuvolos connector package and pandas
+  nuvolos <- import("nuvolos")
+  pd <- import("pandas")
+  
   # using nuvolos python package's to_sql() method
   return(nuvolos$to_sql(df=dbname, name=name, con=con, database=database, schema=schema,
                  if_exists="fail", index=TRUE, index_label=NULL, nanoseconds=FALSE))
@@ -33,7 +35,12 @@ dbWriteTable <- function(dbname,
 
 #' @export
 dbExecute <- function(sql, con){
-
+  require(reticulate)
+  
+  #imoporting python-based nuvolos connector package and pandas
+  nuvolos <- import("nuvolos")
+  pd <- import("pandas")
+  
   # using python's execute method on the established connection
   return(con$execute(sql))
 }
