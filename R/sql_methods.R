79d088993ace20615c7ad8ecd59f117733130391
Bence Pipis <bencepipis@Bences-MacBook-Pro.local>
2021-04-23 11:03:24 +0200

    package import changes

--- R/sql_methods.R
+++ R/sql_methods.R
@@ -1,26 +1,26 @@
 source("R/get_connection.R")
 require(reticulate)
 
-# importing python-based nuvolos connector, installing if not installed yet
-nuvolos <- tryCatch({
+# installing python-based nuvolos connector, if not installed yet
+tryCatch({
   import("nuvolos")
 }, error = function(e) {
   if (is_local()){
     py_install("nuvolos", pip =TRUE)
-    return(import("nuvolos"))
   } else {
     py_install("nuvolos-odbc", pip =TRUE)
-    return(import("nuvolos"))
   }
 })
-
-# importing python package pandas
-pd <- import("pandas")
 
 
 #' @export
 dbGetQuery <- function(sql, con){
   require(reticulate)
+  
+  # importing python nuvolos connector package
+  nuvolos <- import('nuvolos')
+  # importing python package pandas
+  pd <- import("pandas")
   
   # using python's pandas.read_sql() method 
   return(pd$read_sql(sql, con))
@@ -38,6 +38,9 @@ dbWriteTable <- function(dbname,
                          nanoseconds=FALSE){
   require(reticulate)
   
+  # importing python nuvolos connector package
+  nuvolos <- import('nuvolos')
+  
   # using nuvolos python package's to_sql() method
   return(nuvolos$to_sql(df=dbname, name=name, con=con, database=database, schema=schema,
                  if_exists="fail", index=TRUE, index_label=NULL, nanoseconds=FALSE))
@@ -46,6 +49,9 @@ dbWriteTable <- function(dbname,
 #' @export
 dbExecute <- function(sql, con){
   require(reticulate)
+
+  # importing python nuvolos connector package
+  nuvolos <- import('nuvolos')
   
   # using python's execute method on the established connection
   return(con$execute(sql))
