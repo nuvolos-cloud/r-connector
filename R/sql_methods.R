c4bb5d4b3c8d2ffab036d1fba67aeb8a7880506e
Bence Pipis <bencepipis@Bences-MacBook-Pro.local>
2021-04-23 09:25:31 +0200

    importing is_local to sql_methods

--- R/sql_methods.R
+++ R/sql_methods.R
@@ -1,13 +1,19 @@
+source("R/get_connection.R")
+require(reticulate)
 
+if (is_local()){
+  py_install("nuvolos", pip =TRUE)
+} else {
+  py_install("nuvolos-odbc", pip =TRUE)
+}
+
+#imoporting python-based nuvolos connector package and pandas
+nuvolos <- import("nuvolos")
+pd <- import("pandas")
 
 #' @export
 dbGetQuery <- function(sql, con){
-  require(reticulate)
-  
-  #imoporting python-based nuvolos connector package and pandas
-  nuvolos <- import("nuvolos")
-  pd <- import("pandas")
-  
+
   # using python's pandas.read_sql() method 
   return(pd$read_sql(sql, con))
 }
@@ -22,12 +28,7 @@ dbWriteTable <- function(dbname,
                          index=TRUE,
                          index_label=NULL,
                          nanoseconds=FALSE){
-  require(reticulate)
-  
-  #imoporting python-based nuvolos connector package and pandas
-  nuvolos <- import("nuvolos")
-  pd <- import("pandas")
-  
+
   # using nuvolos python package's to_sql() method
   return(nuvolos$to_sql(df=dbname, name=name, con=con, database=database, schema=schema,
                  if_exists="fail", index=TRUE, index_label=NULL, nanoseconds=FALSE))
@@ -35,12 +36,7 @@ dbWriteTable <- function(dbname,
 
 #' @export
 dbExecute <- function(sql, con){
-  require(reticulate)
-  
-  #imoporting python-based nuvolos connector package and pandas
-  nuvolos <- import("nuvolos")
-  pd <- import("pandas")
-  
+
   # using python's execute method on the established connection
   return(con$execute(sql))
 }
